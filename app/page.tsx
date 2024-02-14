81752a0dff7e7cb73636f3ca41eabc4f323cdeb0
Aladdin Sonni <aladdin.sonni@gmail.com>
2024-02-15 00:43:52 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center pt-52">
+      <h1 className="text-3xl font-semibold mb-2">Welcome to the home page!</h1>
+
+      <div className="flex gap-4 mt-10"></div>
+    </main>
+  );
+}
